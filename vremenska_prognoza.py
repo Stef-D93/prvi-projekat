13bd2e69877738ddfc38560766b952bab277e963
Stefan Dendić <stefandendic@yahoo.com>
2024-12-13 12:48:55 +0100

    Dodat prvi fajl

---
+++ vremenska_prognoza.py
@@ -0,0 +1,8 @@
+temperatura = int(input("Unesite temperaturu"))
+if temperatura <0 :
+    print("Oprez klizavo!")
+if temperatura >0 :
+    print("Temperatura je iznad 0")
+    if temperatura >30:
+        print("Uključite klima uređaj")
+        
